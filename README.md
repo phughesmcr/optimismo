c27aa638980756274d3cd8ad9086d62c89d8905e
phugh <peter@phugh.es>
2018-09-20 10:18:43 +0100

    4.0.0 Brings optimismo on par with my other modules

--- README.md
+++ README.md
@@ -1,25 +1,193 @@
 # optimismo
 
-Analyse the optimism of a string.
+Analyse the optimism of a string on a scale of 1 - 9
 
 ## Usage
-```Javascript
+```javascript
 const optimismo = require('optimismo');
-const text = "A big long string of text...";
-const opt = optimismo(text);
-console.log(opt)
+// These are the default and recommended options
+const opts = {
+  'encoding': 'binary',
+  'locale': 'US',
+  'logs': 3,
+  'max': Number.POSITIVE_INFINITY,
+  'min': Number.NEGATIVE_INFINITY,
+  'nGrams': [2, 3],
+  'noInt': false,
+  'output': 'lex',
+  'places': undefined,
+  'sortBy': 'freq',
+  'wcGrams': false,
+};
+const str = 'A string of text....';
+const output = optimismo(str, opts);
+console.log(output);  // {OPTIMISM: 4.89}
 ```
-Scale runs from 1 (Completely pessimistic) to 9 (completely optimistic)
-if there are no matches optimismo will return 0
+Scale runs from 1 (Completely pessimistic) to 9 (completely optimistic). 
 
+If there are no matches optimismo will return null.
 
 ## Default Output
 ```javascript
-  4.89
+ { OPTIMISM: 4.89 }
 ```
 ```Markdown
 1 = very pessimistic, 5 = neutral, 9 = very optimistic
 ```
+
+## The Options Object
+
+The options object is optional and provides a number of controls to allow you to tailor the output to your needs. However, for general use it is recommended that all options are left to their defaults.
+
+### "encoding"
+
+**string - valid options: "binary" (default), "frequency", or "percent"**
+
+*N.B - You probably don't want to change this, ever.*
+
+Controls how the lexical value is calculated.
+
+__Binary__ is simply the addition of lexical weights, i.e. word1 + word2 + word3.
+
+__Frequency__ encoding takes the overall wordcount and word frequency into account, i.e. (word frequency / word count) * weight. Note that the encoding option accepts either 'freq' or 'frequency' to enable this option.
+
+Another way to think of binary and frequency encoding is that 'binary' essentially sets all weights to '1', whereas frequency will generate a group norm. This is useful for predictive lexica, for example, when predicting age (see [predictAge](https://github.com/phugh/predictage)) we want to use frequency encoding because we care about the actual number generated - i.e. the lexical value *is* the predicted age. Whereas, when predicting optimism in this module 'binary' encoding is used because the final value doesn't particularly matter, only whether it is above or below 0 to indicate association.
+
+__Percent__ returns the percentage of total (non-unique) tokens matched against the lexicon in each category as a decimal, i.e. 0.48 = 48%.
+
+### 'locale'
+
+**String - valid options: 'US' (default), 'GB'**
+
+The lexicon data is in American English (US), if the string(s) you want to analyse are in International / British English set the locale option to 'GB'.
+
+### 'logs'
+**Number - valid options: 0, 1, 2, 3 (default)**
+Used to control console.log, console.warn, and console.error outputs.
+* 0 = suppress all logs
+* 1 = print errors only
+* 2 = print errors and warnings
+* 3 = print all console logs
+
+### 'max' and 'min'
+
+**Number - accepts floats**
+
+Each item in the lexicon data has an associated weight (number). Use these options to exclude words that have weights beyond a given maximum or minimum threshold.
+
+By default these are set to infinity, ensuring that no words from the lexicon are excluded.
+
+For English, -0.37 (default) will include everything from the lexicon, 0.85 will include nothing.
+
+For Spanish, -0.85 (default) will include everything from the lexicon, 3.32 will include nothing.
+
+### 'nGrams'
+
+**Array - valid options: [ number, number, ...]**
+
+*N.B the lexicon contains unigrams, bigrams, and trigrams. Including a value > 3 makes no sense and will impact performance drastically.*
+
+n-Grams are contiguous pieces of text, bi-grams being chunks of 2, tri-grams being chunks of 3, etc.
+
+Use the nGrams option to include n-gram chunks. For example if you want to include both bi-grams and tri-grams, use like so:
+
+```javascript
+{
+  nGrams: [2, 3]
+}
+```
+
+If you only want to include tri-grams:
+
+```javascript
+{
+  nGrams: [3]
+}
+```
+
+To disable n-gram inclusion, use the following:
+
+```javascript
+{
+  nGrams: [0]
+}
+```
+
+If the number of words in the string is less than the ngram number provided, the option will simply be ignored.
+
+For accuracy it is recommended that n-grams are included, however including n-grams for very long strings can affect performance.
+
+### 'noInt'
+
+**Boolean - valid options: true or false (default)**
+
+The lexica contain intercept values, set noInt to true to ignore these values.
+
+Unless you have a specific need to ignore the intercepts, it is recommended you leave this set to false.
+
+### 'output'
+
+**String - valid options: 'lex' (default), 'matches', 'full'**
+
+'lex' (default) returns an object of lexical values. See 'Defauly Output Example above.
+
+'matches' returns an object with data about matched words. See 'matches output example' below.
+
+'full' returns both of the above in one object with two keys, 'values' and 'matches'.
+
+### 'places'
+
+**Number - valid options between 0 and 20 inclusive.**
+
+Number of decimal places to limit outputted values to.
+
+The default is "undefined" which will simply return the value unchanged.
+
+### 'sortBy'
+
+**String - valid options: 'freq' (default), 'weight', or 'lex'**
+
+If 'output' = 'matches', this option can be used to control how the outputted array is sorted.
+
+'lex' sorts by final lexical value, (N.B. when using binary encoding [see 'encoding' above] the lexical value and the weight are identical.)
+
+'weight' sorts the array by the matched words initial weight.
+
+'freq' (default) sorts by word frequency, i.e. the most used words appear first.
+
+### 'wcGrams'
+
+**boolean - valid options: true or false (default)**
+
+When set to true, the output from the nGrams option will be added to the word count.
+
+For accuracy it is strongly recommended that this is set to false.
+
+## {output: 'matches'} Output Example
+
+```javascript
+{
+  OPTIMISM: {
+    matches: [
+      [ 'magnificent', 1, -192.0206116, -1.3914537072463768 ],
+      [ 'capital', 1, -133.9311307, -0.9705154398550726 ],
+      [ 'note', 3, -34.83417005, -0.7572645663043478 ],
+      [ 'america', 2, -49.21227355, -0.7132213557971014 ],
+      [ 'republic', 1, -75.5720402, -0.5476234797101449 ],
+    ],
+    info: {
+      total_matches: 100,
+      total_unique_matches: 63,
+      total_tokens: 200,
+      percent_matches: 50,
+    }
+  }
+};
+```
+
+The items in each array represent: [0] - the term, [1] - number of times the term appears in string (frequency), [2] - the terms's weight, [3] - its final lexical value.
+
+The final lexical value is affected by which 'encoding' option you're using.
 
 ## Acknowledgements
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * optimismo
- * v2.0.1
+ * v4.0.0
  *
  * Analyse the optimism of a string.
  *
@@ -12,21 +12,25 @@
  * Used under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0
  * Unported licence
  *
- * (C) 2017 P. Hughes
+ * (C) 2017-18 P. Hughes
  * Licence : Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported
  * http://creativecommons.org/licenses/by-nc-sa/3.0/
  *
  * Usage example:
  * const optimismo = require('optimismo');
+ * // These are the default options
  * const opts = {
  *  'encoding': 'binary',
+ *  'locale': 'US',
+ *  'logs': 3,
  *  'max': Number.POSITIVE_INFINITY,
  *  'min': Number.NEGATIVE_INFINITY,
- *  'nGrams': 'true',
+ *  'nGrams': [2, 3],
+ *  'noInt': false,
  *  'output': 'lex',
- *  'places': 9,
+ *  'places': undefined,
  *  'sortBy': 'freq',
- *  'wcGrams': 'false',
+ *  'wcGrams': false,
  * };
  * const str = 'A big long string of text...';
  * const optimism = optimismo(str, opts);
@@ -38,53 +42,28 @@
  * See README.md for help.
  *
  * @param {string} str  input string
- * @param {Object} opts options object
- * @return {(number|Array|Object)} optimism value, array of matches, or both
+ * @param {Object} [opts] options object
+ * @return {Object} optimism value, array of matches, or both
  */
 
 (function() {
   'use strict';
 
-  let async;
-  let lexicon;
-  let simplengrams;
-  let tokenizer;
-  let lexHelpers;
+  // Lexicon data
+  const affectLex = require('./data/affect.json');
+  const futureLex = require('./data/future.json');
 
-  if (typeof require !== 'undefined') {
-    async = require('async');
-    lexHelpers = require('lex-helpers');
-    lexicon = require('./data/lexicon.json');
-    simplengrams = require('simplengrams');
-    tokenizer = require('happynodetokenizer');
-  } else throw new Error('optimismo: required modules not found!');
-
+  // External modules
+  const async = require('async');
+  const trans = require('british_american_translate');
+  const simplengrams = require('simplengrams');
+  const tokenizer = require('happynodetokenizer');
+  const lexHelpers = require('lex-helpers');
   const arr2string = lexHelpers.arr2string;
-  const calcLex = lexHelpers.calcLex;
+  const doLex = lexHelpers.doLex;
+  const doMatches = lexHelpers.doMatches;
   const getMatches = lexHelpers.getMatches;
-  const prepareMatches = lexHelpers.prepareMatches;
   const itemCount = lexHelpers.itemCount;
-
-  /**
-  * @function getAffect
-  * @param  {Array} arr token array
-  * @return {Object}  object of matches
-  */
-  const getAffect = (arr) => {
-    const matches = {};
-    // loop through the lexicon categories
-    const match = [];
-    // loop through words in category
-    const data = lexicon.AFFECT;
-    for (let word in data) {
-      if (data.hasOwnProperty(word) && arr.indexOf(word) > -1) {
-        match.push(data[word]);
-      }
-    }
-    matches.AFFECT = match;
-    // return matches object
-    return matches;
-  };
 
   /**
   * @function getFuture
@@ -93,129 +72,129 @@
   */
   const getFuture = (arr) => {
     const matches = [];
-    const data = Object.keys(lexicon.FUTURE);
-    let i = data.length;
-    while (i--) {
-      let word = data[i];
-      if (arr.indexOf(word) > -1 && matches.indexOf(word) === -1) {
-        matches.push(word);
-      }
+    const words = [...new Set(arr)];
+    for (let word of words) {
+      if (futureLex.includes(word)) matches.push(word);
     }
     return matches;
   };
 
-  const doMatches = (matches, sortBy, wordcount, places, encoding) => {
-    return {
-      OPTIMISM: prepareMatches(matches.AFFECT, sortBy, wordcount, places,
-        encoding),
-    };
-  };
-
-  const doLex = (matches, places, encoding, wordcount) => {
-    // define intercept value
-    return {
-      OPTIMISM: calcLex(matches.AFFECT, 5.037104721, places, encoding,
-        wordcount),
-    };
-  };
-
   /**
+  * Analyse the optimism of a string.
   * @function optimismo
-  * @param {string} str  input string
-  * @param {Object} opts options object
-  * @return {(number|Array|Object)} optimism value, array of matches, or both
+  * @param {string} str     input string
+  * @param {Object} [opts]  options object
+  * @return {Object} optimism value, array of matches, or both
   */
-  function optimismo(str, opts) {
+  const optimismo = (str, opts = {}) => {
+    // default options
+    opts.encoding = (typeof opts.encoding === 'undefined') ? 'binary' : opts.encoding;
+    opts.locale = (typeof opts.locale === 'undefined') ? 'US' : opts.locale;
+    opts.logs = (typeof opts.logs === 'undefined') ? 3 : opts.logs;
+    if (opts.suppressLog) opts.logs = 0; // suppressLog was depreciated in v3.0.0
+    opts.max = (typeof opts.max === 'undefined') ? Number.POSITIVE_INFINITY : opts.max;
+    opts.min = (typeof opts.min === 'undefined') ? Number.NEGATIVE_INFINITY : opts.min;
+    if (typeof opts.max !== 'number' || typeof opts.min !== 'number') {
+      // try to convert to a number
+      opts.min = Number(opts.min);
+      opts.max = Number(opts.max);
+      // check it worked, or else default to infinity
+      opts.max = (typeof opts.max === 'number') ? opts.max : Number.POSITIVE_INFINITY;
+      opts.min = (typeof opts.min === 'number') ? opts.min : Number.NEGATIVE_INFINITY;
+    }
+    opts.nGrams = (typeof opts.nGrams !== 'undefined') ? opts.nGrams : [2, 3];
+    if (!Array.isArray(opts.nGrams)) {
+      if (opts.nGrams == 0) {
+        opts.nGrams = [0];
+      } else {
+        if (opts.logs > 1) {
+          console.warn('optimismo: nGrams option must be an array! Defaulting to [2, 3].');
+        }
+        opts.nGrams = [2, 3];
+      }
+    }
+    opts.noInt = (typeof opts.noInt === 'undefined') ? false : opts.noInt;
+    opts.output = (typeof opts.output === 'undefined') ? 'lex' : opts.output;
+    opts.sortBy = (typeof opts.sortBy === 'undefined') ? 'freq' : opts.sortBy;
+    opts.wcGrams = (typeof opts.wcGrams === 'undefined') ? false : opts.wcGrams;
+    // cache frequently used options
+    const encoding = opts.encoding;
+    const logs = opts.logs;
+    const nGrams = opts.nGrams;
+    const output = opts.output;
+    const places = opts.places;
+    const sortBy = opts.sortBy;
     // no string return null
     if (!str) {
-      console.error('optimismo: no string found. Returning null.');
+      if (logs > 1) console.warn('optimismo: no string found. Returning null.');
       return null;
     }
     // if str isn't a string, make it into one
     if (typeof str !== 'string') str = str.toString();
     // trim whitespace and convert to lowercase
     str = str.trim().toLowerCase();
-    // options defaults
-    if (!opts || typeof opts !== 'object') {
-      console.warn('optimismo: using default options');
-      opts = {
-        'encoding': 'binary',
-        'max': Number.POSITIVE_INFINITY,
-        'min': Number.NEGATIVE_INFINITY,
-        'nGrams': 'true',
-        'output': 'lex',
-        'places': 9,
-        'sortBy': 'freq',
-        'wcGrams': 'false',
-      };
-    }
-    opts.encoding = opts.encoding || 'binary';
-    opts.max = opts.max || Number.POSITIVE_INFINITY;
-    opts.min = opts.min || Number.NEGATIVE_INFINITY;
-    opts.nGrams = opts.nGrams || 'true';
-    opts.output = opts.output || 'lex';
-    opts.places = opts.places || 9;
-    opts.sortBy = opts.sortBy || 'freq';
-    opts.wcGrams = opts.wcGrams || 'false';
-    const encoding = opts.encoding;
-    const output = opts.output;
-    const places = opts.places;
-    const sortBy = opts.sortBy;
+    // translalte US English to UK English if selected
+    if (opts.locale.match(/gb/gi)) str = trans.uk2us(str);
     // convert our string to tokens
-    let tokens = tokenizer(str);
+    let tokens = tokenizer(str, {logs: opts.logs});
     // if there are no tokens return null
     if (!tokens) {
-      console.warn('optimismo: no tokens found. Returned null.');
+      if (logs > 1) console.warn('optimismo: no tokens found. Returned null.');
       return null;
     }
     // get wordcount before we add ngrams
     let wordcount = tokens.length;
     // get n-grams
-    if (opts.nGrams === 'true' && wordcount > 2) {
-      async.each([2, 3], function(n, callback) {
-        tokens = tokens.concat(
-          arr2string(simplengrams(str, n))
-        );
-        callback();
+    if (nGrams && !nGrams.includes(0)) {
+      async.each(nGrams, function(n, callback) {
+        if (wordcount < n) {
+          callback(`wordcount (${wordcount}) less than n-gram value (${n}). Ignoring.`);
+        } else {
+          tokens = [
+            ...arr2string(simplengrams(str, n, {logs: logs})),
+            ...tokens,
+          ];
+          callback();
+        }
       }, function(err) {
-        if (err) console.error(err);
+        if (err && logs > 1) console.warn('optimismo: ', err);
       });
     }
     // recalculate wordcount if wcGrams is true
-    if (opts.wcGrams === 'true') wordcount = tokens.length;
-    // get 'future' match tokens
-    let future = getFuture(tokens);
-    // reduce tokens to count item
-    future = itemCount(future);
+    if (opts.wcGrams === true) wordcount = tokens.length;
+    // reduce 'future' match tokens to count item
+    const future = itemCount(getFuture(tokens));
     // match future tokens against affect lexicon
-    const matches = getMatches(future, lexicon, opts.min, opts.max);
+    const matches = getMatches(future, affectLex, opts.min, opts.max);
+    let ints = {OPTIMISM: 5.037104721};
+    if (opts.noInt === true) ints.OPTIMISM = 0;
     // output!
-    if (output === 'matches') {
-      return doMatches(matches, sortBy, wordcount, places, encoding);
-    } else if (output === 'full') {
-      let full = {};
-      async.parallel([
-        function(callback) {
-          full.matches = doMatches(matches, sortBy, wordcount, places,
-              encoding);
-          callback();
+    if (output.match(/matches/gi)) {
+      // return requested output
+      return doMatches(matches, encoding, wordcount, sortBy, places);
+    } else if (output.match(/full/gi)) {
+      // return matches and values in one object
+      let results;
+      async.parallel({
+        matches: function(callback) {
+          callback(null, doMatches(matches, encoding, wordcount, sortBy, places));
         },
-        function(callback) { 
-          full.values = doLex(matches, places, encoding, wordcount);
-          callback();
+        values: function(callback) {
+          callback(null, doLex(matches, ints, encoding, wordcount, places));
         },
-      ], function(err) {
-          if (err) console.error(err);
-          return full;
-      });    
+      }, function(err, res) {
+        if (err && logs > 0) console.error(err);
+        results = res;
+      });
+      return results;
     } else {
-      if (output !== 'lex') {
-        console.warn('optimismo: output option ("' + output +
-            '") is invalid, defaulting to "lex".');
+      if (!output.match(/lex/gi) && logs > 1) {
+        console.warn(`optimismo: output option ("${output}") is invalid, defaulting to "lex".`);
       }
-      return doLex(matches, places, encoding, wordcount);
+      // return just the values
+      return doLex(matches, ints, encoding, wordcount, places);
     }
-  }
+  };
 
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
@@ -223,6 +202,6 @@
     }
     exports.optimismo = optimismo;
   } else {
-    root.optimismo = optimismo;
+    global.optimismo = optimismo;
   }
 })();
